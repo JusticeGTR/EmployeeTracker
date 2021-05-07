025d10ac38802d9a59098f7c059addb260d5c8d6
Justin Lindsey <justinlindseylhr@gmail.com>
2021-05-07 09:44:18 -0500

    updating

--- schema.sql
+++ schema.sql
@@ -6,7 +6,7 @@ USE employee_tracker;
 
 CREATE TABLE department(
   id INT NOT NULL AUTO_INCREMENT,
-  NAME VARCHAR(30),
+  name VARCHAR(30),
   PRIMARY KEY (id)
 
 );
--- server.js
+++ server.js
@@ -1,142 +1,192 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql');
 const cTable = require('console.table');
+//require('dotenv').config();
+console.log(require('dotenv').config());
 
-const connection = mysql.createConnection(
-  
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    port: 3306,
-  });
+// const connection = mysql.createConnection(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3306,
+//   });
 
-  const start = async () => {
-    const answer = await inquirer
-      .prompt({
-        name: 'initialChoice',
-        type: 'list',
-        message: 'What would you like to do?',
-        choices: [ "Add departments", "Add Roles", "Add employees", "View deparments", "View roles", "View employees", "View employees by manager", "Update employee managers", "Delete departments", "Delete roles", "Delete employees", "Exit",
-      //   {
-      //     name : "Justin Lindsey",
-      //     value : 1 
-      // }
-],
-      })
-      console.log(answer)
-      switch(answer.initialChoice) {
-        case "Add departments":
-          addDepartment();
-          break;
-        case "Add Roles":
-          addRole();
-          break;
-        case "Add employees":
-          addEmployee();
-          break;
-        case "View departments":
-          viewDepartment();
-          break;
-        case "View Roles":
-          viewRoles();
-          break;
-        case "View employees":
-          viewEmployee();
-          break;
-        case "View employees by manager":
-          employeesByManager();
-          break;
-        case "Update employee managers":
-          updateManager();
-          break;
-        case "Deplete departments":
-          depleteDepartment();
-          break;
-        case "Delete roles":
-          deleteRole();
-          break;
-        case "Delete Employees":
-          deleteEmployee();
-          break;
-        case "Exit":
-          console.log("Have a nice day!");
-      }
-    //   .then((answer) => {
+const connection = mysql.createConnection({
+  host: 'localhost',
+  // Your port; if not 3306
+  port: 3306,
+  // Your username
+  user: process.env.DB_USER,
+  // Your password
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
-     
-    //     }
-    //   });
-  };
+const start = async () => {
+  const answer = await inquirer
+    .prompt({
+      name: 'initialChoice',
+      type: 'list',
+      message: 'What would you like to do?',
+      choices: ["Add Departments", "Add Roles", "Add Employees", "View Deparments", "View Roles", "View Employees", "View Employees By Manager", "Update Employee Managers", "Delete Departments", "Delete Roles", "Delete Employees", "Exit",
+        //   {
+        //     name : "Justin Lindsey",
+        //     value : 1 
+        // }
+      ],
+    })
+  console.log(answer)
+  switch (answer.initialChoice) {
+    case "Add departments":
+      addDepartment();
+      break;
+    case "Add Roles":
+      addRole();
+      break;
+    case "Add employees":
+      addEmployee();
+      break;
+    case "View departments":
+      viewDepartment();
+      break;
+    case "View Roles":
+      viewRoles();
+      break;
+    case "View employees":
+      viewEmployee();
+      break;
+    case "View employees by manager":
+      employeesByManager();
+      break;
+    case "Update employee managers":
+      updateManager();
+      break;
+    case "Deplete departments":
+      depleteDepartment();
+      break;
+    case "Delete roles":
+      deleteRole();
+      break;
+    case "Delete Employees":
+      deleteEmployee();
+      break;
+    case "Exit":
+      console.log("Have a nice day!");
+  }
+  //   .then((answer) => {
 
-  const addDepartment = async () => {
+
+  //     }
+  //   });
+};
+
+const addDepartment = async () => {
+  const answer = await inquirer
+    .prompt([
+      {
+        name: 'department',
+        type: 'input',
+        message: 'What department would you like to add?',
+      },
+    ])
+  connection.query(
+    'INSERT INTO department SET ?',
+    {
+      name: answer.department,
+    },
+    (err) => {
+      if (err) throw err;
+      console.log('Your department was added successfully!');
+      console.log()
+      start();
+    }
+  );
+};
+
+const addRole = () => {
+  //gather all the departments as an array to pass into the roleDepartment prompt
+  connection.query('SELECT * FROM department', async (err, res) => {
+    const departmentChoices = res.map(dept => {
+      return {name: dept.name, value: dept.id}
+    });
+    
     const answer = await inquirer
       .prompt([
         {
-          name: 'department',
+          name: 'role',
           type: 'input',
-          message: 'What department would you like to add?',
+          message: 'What is the title of the role that you would like to add?',
         },
+        {
+          name: 'salary',
+          type: 'input',
+          message: 'What is the salary of this role?'
+        },
+        {
+          name: 'roleDepartment',
+          type: 'list',
+          message: 'Which department does this belong in?',
+          choices: departmentChoices
+        }
       ])
-        connection.query(
-          'INSERT INTO department SET ?',
-          {
-            name: answer.department,
-          },
-          (err) => {
-            if (err) throw err;
-            console.log('Your department was added successfully!');
-            console.log()
-            start();
-          }
-        );
-  };
 
-  const addRole = async () => {
-    //gather all the departments as an array to pass into the roleDepartment prompt
-    connection.query('SELECT * FROM department', (err, res) => {
-      // const departmentChoices = res.something is all the departments
-      const answer = await inquirer
-        .prompt([
-          {
-            name: 'role',
-            type: 'input',
-            message: 'What is the title of the role that you would like to add?',
-          },
-          {
-            name: 'salary',
-            type: 'input',
-            message: 'What is the salary of this role?'
-          }
-          {
-            name: 'roleDepartment',
-            type: 'list',
-            message: 'Which department does this belong in?',
-            choices: departmentChoices
-          }
-        ])
+    connection.query(
+      'INSERT INTO role SET ?',
+      {
+        title: answer.role,
+        salary: answer.salary,
+        department_id: answer.roleDepartment,
+      },
+      (err) => {
+        if (err) throw err;
+        console.log('Your department was added successfully!');
+        console.log()
+        start();
+      }
+    );
+  })
+};
 
-    })
-        connection.query(
-          'INSERT INTO role SET ?',
-          {
-            name: answer.department,
-          },
-          (err) => {
-            if (err) throw err;
-            console.log('Your department was added successfully!');
-            console.log()
-            start();
-          }
-        );
-  };
+const addEmployee = async () => {
+  const answer = await inquirer
+    .prompt([
+      {
+        name: 'firstName',
+        type: 'input',
+        message: 'What is the new employee\'s first name?',
+      },
+      {
+        name: 'lastName',
+        type: 'input',
+        message: 'What is the new employee\'s last name?',
+      },
+      
+    ])
+  connection.query(
+    'INSERT INTO department SET ?',
+    {
+      name: answer.department,
+    },
+    (err) => {
+      if (err) throw err;
+      console.log('Your department was added successfully!');
+      console.log()
+      start();
+    }
+  );
+};
 
-  connection.connect((err) => {
-    if (err) throw err;
-    start();
-  });
-  
+
+
+
+connection.connect((err) => {
+  if (err) throw err;
+  start();
+});
+
 
 
 
